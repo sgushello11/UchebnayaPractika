397b128796261b889ec8adf0e442973ab8ea36c8
Gush <loveg6448@gmail.com>
2026-04-17 12:27:21 +0300

    Create MyRequestsWindow.xaml.cs

---
+++ 3 day/HranitelPro/MyRequestsWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace HranitelPro
+{
+    public partial class MyRequestsWindow : Window
+    {
+        private DatabaseHelper db = new DatabaseHelper();
+        private MainWindow mainWindow;
+        private User currentUser;
+
+        public MyRequestsWindow(MainWindow main, User user)
+        {
+            InitializeComponent();
+            mainWindow = main;
+            currentUser = user;
+
+            var list = db.GetUserRequests(currentUser.UserID);
+            dgvRequests.ItemsSource = list;
+
+            btnBack.Click += (s, e) => { mainWindow.Show(); this.Close(); };
+        }
+    }
+}
