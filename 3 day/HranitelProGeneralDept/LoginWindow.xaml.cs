16199ed3f09d1bf376aba1ce26d067f934fbd3e3
Gush <loveg6448@gmail.com>
2026-04-17 12:32:34 +0300

    Create LoginWindow.xaml.cs

---
+++ 3 day/HranitelProGeneralDept/LoginWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace HranitelProGeneralDept
+{
+    public partial class LoginWindow : Window
+    {
+        private DatabaseHelper db = new DatabaseHelper();
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+            btnLogin.Click += BtnLogin_Click;
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string code = txtCode.Password.Trim();
+            var user = db.LoginByEmployeeCode(code);
+
+            if (user != null && user.Department == "Общий отдел")
+            {
+                var mainWindow = new MainWindow(user);
+                mainWindow.Show();
+                this.Close();
+            }
+            else if (user != null && user.Department != "Общий отдел")
+            {
+                lblError.Text = "Доступ только для сотрудников общего отдела";
+            }
+            else
+            {
+                lblError.Text = "Неверный код сотрудника";
+            }
+        }
+    }
+}
