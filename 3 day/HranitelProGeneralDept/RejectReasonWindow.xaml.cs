8da92da3d936222f3f1379fe4eb836f8ae4fbaf9
Gush <loveg6448@gmail.com>
2026-04-17 12:35:30 +0300

    Create RejectReasonWindow.xaml.cs

---
+++ 3 day/HranitelProGeneralDept/RejectReasonWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HranitelProGeneralDept
+{
+    public partial class RejectReasonWindow : Window
+    {
+        public string RejectionReason { get; private set; } = "";
+
+        public RejectReasonWindow()
+        {
+            InitializeComponent();
+
+            cmbReason.SelectionChanged += (s, e) =>
+            {
+                var selected = (cmbReason.SelectedItem as ComboBoxItem)?.Content.ToString();
+                txtOtherReason.Visibility = (selected == "Другое") ? Visibility.Visible : Visibility.Collapsed;
+            };
+
+            btnOk.Click += (s, e) =>
+            {
+                var selected = (cmbReason.SelectedItem as ComboBoxItem)?.Content.ToString();
+                if (selected == "Другое")
+                {
+                    if (string.IsNullOrWhiteSpace(txtOtherReason.Text))
+                    {
+                        lblError.Text = "Введите причину отклонения";
+                        return;
+                    }
+                    RejectionReason = txtOtherReason.Text;
+                }
+                else
+                {
+                    RejectionReason = selected ?? "";
+                }
+
+                DialogResult = true;
+                Close();
+            };
+
+            btnCancel.Click += (s, e) => Close();
+
+            cmbReason.SelectedIndex = 0;
+        }
+    }
+}
