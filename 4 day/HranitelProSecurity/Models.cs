f6a1d860e9f28da3d03e7c65666c27852bc1e1b6
Gush <loveg6448@gmail.com>
2026-04-18 10:56:25 +0300

    Create Models.cs

---
+++ 4 day/HranitelProSecurity/Models.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HranitelProSecurity
+{
+    public class User
+    {
+        public int EmployeeID { get; set; }
+        public string LastName { get; set; } = "";
+        public string FirstName { get; set; } = "";
+        public string Patronymic { get; set; } = "";
+        public string EmployeeCode { get; set; } = "";
+        public string Department { get; set; } = "";
+        public string FullName => $"{LastName} {FirstName} {Patronymic}".Trim();
+    }
+
+    public class VisitRequest
+    {
+        public int RequestID { get; set; }
+        public string RequestType { get; set; } = "";
+        public string Status { get; set; } = "";
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string VisitPurpose { get; set; } = "";
+        public string TargetDepartment { get; set; } = "";
+        public string VisitorLastName { get; set; } = "";
+        public string VisitorFirstName { get; set; } = "";
+        public string VisitorPatronymic { get; set; } = "";
+        public string VisitorPassportData { get; set; } = "";
+        public string VisitorPhone { get; set; } = "";
+        public DateTime? ActualEntryTime { get; set; }
+        public DateTime? ActualExitTime { get; set; }
+        public string FullName => $"{VisitorLastName} {VisitorFirstName} {VisitorPatronymic}".Trim();
+    }
+}
